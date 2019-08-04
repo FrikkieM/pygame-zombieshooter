2b5ba82001b9569419d7844e7493958c23c9c27e
Frikkie van der Merwe <frikkieweb@gmail.com>
2019-08-04 17:01:48 +0200

    End of Part 2

---
+++ sprites.py
@@ -0,0 +1,43 @@
+import pygame as pg 
+from settings import *
+
+class Player(pg.sprite.Sprite):
+    def __init__(self, game, x, y):
+        self.groups = game.all_sprites
+        pg.sprite.Sprite.__init__(self, self.groups)
+        self.game = game
+        self.image = pg.Surface((TILESIZE,TILESIZE))
+        self.image.fill(YELLOW)
+        self.rect = self.image.get_rect()
+        self.x = x
+        self.y = y
+
+    def move(self, dx=0, dy=0):
+        if not self.collide_with_walls(dx,dy):
+            self.x += dx
+            self.y += dy
+
+    def collide_with_walls(self, dx=0, dy=0):
+        #Will return True if we are trying to move into a x,y where there is a wall, else
+        #return False - meaning no collision detected.
+        for wall in self.game.walls:
+            if wall.x == self.x + dx and wall.y == self.y + dy:
+                return True
+        return False
+
+    def update(self):
+        self.rect.x = self.x * TILESIZE     #To find upper left x pixel to draw rect in correct grid
+        self.rect.y = self.y * TILESIZE
+        
+class Wall(pg.sprite.Sprite):
+    def __init__(self, game, x, y):
+        self.groups = game.all_sprites, game.walls
+        pg.sprite.Sprite.__init__(self, self.groups)
+        self.game = game
+        self.image = pg.Surface((TILESIZE, TILESIZE))
+        self.image.fill(GREEN)
+        self.rect = self.image.get_rect()
+        self.x = x
+        self.y = y
+        self.rect.x = x * TILESIZE
+        self.rect.y = y * TILESIZE
